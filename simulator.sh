600a1b84a58c231a52bb90759c07f4d7e60d3cf4
vitaliybizilia <biziliavv@gmail.com>
2016-12-01 14:52:04 +0200

    Simulator

--- simulator.sh
+++ simulator.sh
@@ -6,7 +6,7 @@ xcrun simctl erase all
 #open the iphonesimulator and kill it
 #this is required after a system restart
 #so the simulator knows to run iPad rather than iPhone apps
-xcrun instruments -w "iPhone 6s (10.1) [404602A6-7722-485A-BB64-11DE7EE45A42] (Simulator)"
+open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app
 sleep 10
 
 
