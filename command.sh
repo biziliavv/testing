13c9e614a37c6ec917399be8bc7805d1ba9b00ec
vitaliybizilia <biziliavv@gmail.com>
2016-12-01 14:12:01 +0200

    Test

--- command.sh
+++ command.sh
@@ -1,4 +1,4 @@
-xcodebuild test -scheme LPSimpleExampleUITests -destination "platform=iOS Simulator,name=iPhone 6,OS=latest" -sdk iphonesimulator10.1 | xcpretty --report html
+xcodebuild test -scheme LPSimpleExample -destination "platform=iOS Simulator,name=iPhone 6,OS=latest" -sdk iphonesimulator10.1 | xcpretty --report html
 
-xcodebuild test -scheme LPSimpleExampleUITests -destination "platform=iOS Simulator,name=iPhone 6s,OS=latest" -sdk iphonesimulator10.1 | xcpretty --report html
+xcodebuild test -scheme LPSimpleExample -destination "platform=iOS Simulator,name=iPhone 6s,OS=latest" -sdk iphonesimulator10.1 | xcpretty --report html
 
