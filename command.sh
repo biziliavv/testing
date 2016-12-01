79cf2496a4320bc2965dd99f3b1a9636ff88b20c
biziliavv <biziliavv@gmail.com>
2016-12-01 15:59:14 +0200

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-xcodebuild test -scheme LPSimpleExample -destination "platform=iOS Simulator,name=iPhone 6,OS=10.1" -sdk iphonesimulator10.1 | xcpretty --report html --output iphone6.html
-xcodebuild test -scheme LPSimpleExample -destination "platform=iOS Simulator,name=iPhone 6s,OS=10.1" -sdk iphonesimulator10.1 | xcpretty --report html --output iphone6s.html
+xcodebuild test -scheme LPSimpleExample -destination "platform=iOS Simulator,name=iPhone 6,OS=10.1" -sdk iphonesimulator10.1 | xcpretty --report html --output build/reports/iphone6.html
+xcodebuild test -scheme LPSimpleExample -destination "platform=iOS Simulator,name=iPhone 6s,OS=10.1" -sdk iphonesimulator10.1 | xcpretty --report html --output build/reports/iphone6s.html
 
 #delete previous build folders
 
