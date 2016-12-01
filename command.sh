99d25e40b10b338b16635e8e432583dd6109de69
biziliavv <biziliavv@gmail.com>
2016-12-01 15:27:55 +0200

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-xcodebuild test -scheme LPSimpleExample -destination "platform=iOS Simulator,name=iPhone 6,OS=10.1" -sdk iphonesimulator10.1 | xcpretty --report html
+xcodebuild test -scheme LPSimpleExample -destination "platform=iOS Simulator,name=iPhone 6,OS=10.1" -sdk iphonesimulator10.1
 
 #delete previous build folders
-echo "Removing previous build folder"
-rm -r ${WORKSPACE}/build
-mkdir ${WORKSPACE}/build
 
