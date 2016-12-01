91872ef6c7a6ac53acfd48e6b4b24e70803ed43f
vitaliybizilia <biziliavv@gmail.com>
2016-12-01 14:38:47 +0200

    Test

--- command.sh
+++ command.sh
@@ -6,9 +6,9 @@ xcrun simctl erase all
 #open the iphonesimulator and kill it
 #this is required after a system restart
 #so the simulator knows to run iPad rather than iPhone apps
-xcrun instruments -w "iPhone 6s (10.1 Simulator)"
+xcrun instruments -w "iPhone 6s (10.1) [404602A6-7722-485A-BB64-11DE7EE45A42] (Simulator)"
 sleep 10
-xcodebuild test -scheme LPSimpleExampleUITests -destination "platform=iOS Simulator,name=iPhone 6,OS=latest" -sdk iphonesimulator10.1
+xcodebuild test -scheme LPSimpleExampleUITests -destination "platform=iOS Simulator,name=iPhone 6,OS=10.1" -sdk iphonesimulator10.1
 
 #delete previous build folders
 echo "Removing previous build folder"
