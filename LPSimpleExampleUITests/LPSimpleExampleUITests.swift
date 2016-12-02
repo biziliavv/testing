33ee6955b67570b74db673f1f81711870a14b1c1
Vitaliy Bizilia <tester.bizilia@gmail.com>
2016-12-02 12:02:52 +0200

    fixed

--- LPSimpleExampleUITests/LPSimpleExampleUITests.swift
+++ LPSimpleExampleUITests/LPSimpleExampleUITests.swift
@@ -66,7 +66,7 @@ class LPSimpleExampleUITests: XCTestCase {
         app.buttons["tr"].tap()
         app.buttons["bl"].tap()
         app.buttons["br"].tap()
-        app.buttons["BLA:42"].tap()
+        app.buttons["BAL:42"].tap()
     }
     func testLastScreen(){
         
